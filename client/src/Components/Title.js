5526af1053dc825158cbee3f9fc3582d0a4c2133
asiagromasia <35482401+asiagromasia@users.noreply.github.com>
2020-03-12 17:07:16 -0700

    Create Title.js

---
+++ client/src/Components/Title.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+class Title extends Component{   
+    render(){   
+        return(<h1>Welcome to our Restaurant!</h1>
+              )
+    }
+}                
+export default Title;
